568c906e41b033187b589758af72a5254bbb7c88
Zowie Erickson <zowie.erickson@gmail.com>
2021-05-29 22:03:05 -0400

    feat: showPage function displaying all employees; addPagination function displaying pagination bttons

--- js/script.js
+++ js/script.js
@@ -1,24 +1,67 @@
-/*
-Treehouse Techdegree:
-FSJS Project 2 - Data Pagination and Filtering
-*/
+// Selectors
+const studentList = document.querySelector(".student-list");
+const linkList = document.querySelector(".link-list");
 
 /*
-For assistance:
-   Check out the "Project Resources" section of the Instructions tab: https://teamtreehouse.com/projects/data-pagination-and-filtering#instructions
-   Reach out in your Slack community: https://treehouse-fsjs-102.slack.com/app_redirect?channel=unit-2
+Create and insert/append the elements needed to display a "page" of nine students
 */
+
+function showPage(list, page) {
+  const startIndex = page * 9 - 9;
+  const endIndex = page * 9;
+  studentList.innerHTML = "";
+
+  for (let i = 0; i < list.length; i++) {
+    let firstName = list[i].name.first;
+    let lastName = list[i].name.last;
+    let picture = list[i].picture.large;
+    let email = list[i].email;
+    let register = list[i].registered.date;
+    if (i >= startIndex && i < endIndex) {
+      studentList.insertAdjacentHTML(
+        "beforeend",
+        `  
+       <li class="student-item cf">
+       <div class="student-details">
+         <img class="avatar" src="${picture}" alt="Profile Picture">
+         <h3>${firstName} ${lastName}</h3>
+         <span class="email">${email}</span>
+       </div>
+       <div class="joined-details">
+         <span class="date">Joined ${register}</span>
+       </div>
+     </li>`
+      );
+    }
+  }
+}
 
 /*
-Create the `showPage` function
-This function will create and insert/append the elements needed to display a "page" of nine students
+Create and insert/append the elements needed for the pagination buttons
 */
 
-function showPage() {}
+function addPagination(list) {
+  linkList.innerHTML = `<li>
+  <button type="button" class="active">1</button>
+</li>`;
+  for (let i = 2; i < list.length / 9 + 1; i++) {
+    linkList.insertAdjacentHTML(
+      "beforeend",
+      ` <li>
+    <button type="button">${i}</button>
+  </li>`
+    );
+  }
+}
 
-/*
-Create the `addPagination` function
-This function will create and insert/append the elements needed for the pagination buttons
-*/
+linkList.addEventListener("click", (e) => {
+  e = e.target;
+  if (e.tagName === "BUTTON") {
+    console.log("You clicked a button!");
+  }
+});
 
 // Call functions
+
+showPage(data, 5);
+addPagination(data);
